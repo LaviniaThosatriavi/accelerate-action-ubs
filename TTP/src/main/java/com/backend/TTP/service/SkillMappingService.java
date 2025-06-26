770d2ebfb6b390833ed627bce0bb20d4f2e6214a
Lavinia Thosatriavi <lthosatriavi@gmail.com>
2025-06-26 16:37:03 +0700

    Add Docker configuration for Render deployment

--- TTP/src/main/java/com/backend/TTP/service/SkillMappingService.java
+++ TTP/src/main/java/com/backend/TTP/service/SkillMappingService.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import com.backend.TTP.model.UserProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
